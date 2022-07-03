b60af03b54ca0d1593159725e1a8e6fd96e7d682
Gemma Cameron <gemma.cameron@gmail.com>
2022-07-03 13:20:57 +0100

    missing bits added to get react working

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 4000;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/express_backend', (req, res) => { //Line 9
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 app.listen(port, () => {
